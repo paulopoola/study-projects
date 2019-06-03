5da181dfc2e2f474e30a7745a895ef88cc7f4ad6
Opoola Paul <paul.opoola@gmail.com>
2019-06-03 17:45:06 +0300

    Add files via upload

---
+++ HandlingErrorExceptions.py
@@ -0,0 +1,64 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+for i in ['a', 'b', 'c']:
+    print(i**2)
+
+
+# In[3]:
+
+
+try:
+    for i in ['a', 'b', 'c']:
+        print(i**2)
+except TypeError:
+    print('The List consist of strings')
+    
+
+
+# In[5]:
+
+
+try:
+    x = 5
+    y = 0
+    z = x/y
+except ZeroDivisionError:
+    print('Cannot be divisible by Zero')
+finally:
+    print('All Done')
+
+
+# In[ ]:
+
+
+
+
+
+
+# In[9]:
+
+
+def ask():
+    while True:
+        try:
+            number = 0
+            number = int(input('Write a number here: '))
+        except ValueError:
+            print('You didnt input a correct number')
+            break
+        else:
+            a = number**2
+            print('{} is the square of your number, {}'.format(a, number))
+        finally:
+            print('Thank You')
+
+
+# In[10]:
+
+
+ask()
+
